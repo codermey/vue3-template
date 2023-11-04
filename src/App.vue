f0decc4049f349fb30da7debf5cb76a0a39ac6a1
codermey <18672267024@163.com>
2023-11-04 23:40:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="You did it!" />
+  <RouterView />
+</template>
