aa80c0a01635b19b310c3b308c95d32fde314d05
codermey <18672267024@163.com>
2023-11-05 19:59:29 +0800

    feat: UnoCSS集成

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import 'reset-css'
+// import 'reset-css'
+import 'virtual:uno.css'
+import '@unocss/reset/tailwind-compat.css'
 
 const app = createApp(App)
 
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,9 @@
 <template>
+  <h1>hello world</h1>
   <div class="home">This is the home page</div>
+  <div class="m-4">Hello</div>
+
+  <div class="container mx-auto px-4">Hello TailwindCSS</div>
 </template>
 
 <script setup lang="ts"></script>
