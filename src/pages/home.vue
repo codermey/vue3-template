5a2296d72287c52f962749f239625c339e39c747
codermey <18672267024@163.com>
2023-11-05 20:31:17 +0800

    feat: unplugin-auto-import &  VueUse集成

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,8 +4,17 @@
   <div class="m-4">Hello</div>
 
   <div class="container mx-auto px-4">Hello TailwindCSS</div>
+
+  <div class="text-blue-500">{{ msg }}</div>
+
+  <p>x:{{ x }} y:{{ y }}</p>
+  <p>sourceType:{{ sourceType }}</p>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const msg = ref('my name is codermey')
+
+const { x, y, sourceType } = useMouse()
+</script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,34 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import AutoImport from 'unplugin-auto-import/vite'
+
 import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    VueRouter({
-      /* options */
-    }),
+    VueRouter({}),
     vue(),
     vueJsx(),
-    UnoCSS()
+    UnoCSS(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+      imports: [
+        // presets
+        'vue',
+        '@vueuse/core',
+        VueRouterAutoImports
+      ]
+    })
   ],
   resolve: {
     alias: {
